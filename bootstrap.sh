cbdf7f0aed0ed959ec4c8d221cd9ee7118b0c818
Jonny An <jan@esri.com>
2017-03-13 09:27:03 -0700

    use master branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@ fi
 # Fetch my dotfiles
 if [[ ! -d $dotfiles ]]; then
 	printf '>> Fetching dotfiles\n'
-	git clone -b zsh --recursive $dotfiles_repo $dotfiles
+	git clone --recursive $dotfiles_repo $dotfiles
 fi
 
 # cd ~/.dotfile
