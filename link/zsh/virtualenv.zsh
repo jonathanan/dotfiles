719d44fcfd0931e9bc3dd29476c3e46c0c3a6991
Jonny An <jonnyleean@gmail.com>
2021-02-10 20:11:40 -0800

    zsh: virtualenv python3 path

--- link/zsh/virtualenv.zsh
+++ link/zsh/virtualenv.zsh
@@ -1,4 +1,4 @@
 export WORKON_HOME=~/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_PYTHON=/opt/homebrew/bin/python3
 source /opt/homebrew/bin/virtualenvwrapper.sh
